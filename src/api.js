881da206279e492624fb86da099a586a93877bb8
EtienneDTS <detoisienetienne@gmail.com>
2025-05-21 14:11:38 +0200

    fetch one product func

--- src/api.js
+++ src/api.js
@@ -40,3 +40,22 @@ export const loginUser = async (email, password) => {
     throw err;
   }
 };
+
+export const getOneProduct = async (id) => {
+    
+  try {
+    const response = await fetch(`${BASEURL}/produits/${id}`, {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      }
+    });
+    if (!response.ok) throw new Error(`Erreur API : ${response.status}`);
+    const data = await response.json();
+    console.log("data", data)
+    return Array.isArray(data) ? data : {};
+  } catch (err) {
+    console.error("Erreur lors du fetch du produit :", err);
+    return [];
+  }
+};
